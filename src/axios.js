bbe4752d3833942937dfe3dba955a38ec539489a
kapil <pkapil@users.noreply.github.com>
2023-11-04 18:15:33 -0500

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://udtnjoycqs.us17.qoddiapp.com/",
+  baseURL: "https://udtnjoycqs.us17.qoddiapp.com/",
   timeout: 10000,
 });
 
